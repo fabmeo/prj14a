8f98792c29e09687689cb4d3e5cb58926605de00
fabmeo <164397533+fabmeo@users.noreply.github.com>
2024-11-09 00:10:40 +0100

    prime views

--- src/albdif/urls.py
+++ src/albdif/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # ex: /prop/5/
+    path("proprieta/<int:proprieta_id>/", views.proprieta_detail, name="prop_detail"),
+    # ex: /prop/5/camera/
+    path("camera/<int:camera_id>/", views.camera_detail, name="camera_detail"),
 ]
--- src/albdif/views.py
+++ src/albdif/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def proprieta_detail(request, proprieta_id):
+    return HttpResponse("Dettaglio della proprietà %s." % proprieta_id)
+
+
+def camera_detail(request, camera_id):
+    response = "Questa è la camera %s."
+    return HttpResponse(response % camera_id)
