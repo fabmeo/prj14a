54b9d61263a9c87d4939becfe7acdef555987a93
fabmeo <164397533+fabmeo@users.noreply.github.com>
2024-11-09 16:12:31 +0100

    prime views

--- src/albdif/urls.py
+++ src/albdif/urls.py
@@ -4,8 +4,21 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # ex: /prop/5/
+    # ex: /proprieta/1/
     path("proprieta/<int:proprieta_id>/", views.proprieta_detail, name="prop_detail"),
-    # ex: /prop/5/camera/
+
+    # ex: /proprieta/
+    path("proprieta/", views.proprieta_list, name="proprieta_list"),
+    # ex: /proprieta/1/
+    path("proprieta/<int:proprieta_id>/", views.proprieta_detail, name="proprieta_detail"),
+
+    # ex: /camere/
+    path("camere/", views.camere_list, name="camere_list"),
+    # ex: /camera/1/
     path("camera/<int:camera_id>/", views.camera_detail, name="camera_detail"),
+
+    # ex: /stagioni/
+    path("stagioni/", views.stagioni_list, name="stagioni_list"),
+    # ex: /stagione/5/
+    path("stagione/<int:stagione_id>/", views.stagione_detail, name="stagione_detail"),
 ]
--- src/albdif/views.py
+++ src/albdif/views.py
@@ -1,17 +1,45 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
 
-# Create your views here.
-from django.http import HttpResponse
+from .models import Stagione, Camera, Proprieta
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
+# PROPRIETA'
 def proprieta_detail(request, proprieta_id):
-    return HttpResponse("Dettaglio della proprietà %s." % proprieta_id)
+    response = "Questa è la proprieta %s."
+    return HttpResponse(response % proprieta_id)
 
 
+def proprieta_list(request):
+    w_proprieta_list = Proprieta.objects.order_by("descrizione")[:5]
+    context = {"proprieta_list": w_proprieta_list}
+    return render(request, "albdif/proprieta_list.html", context)
+
+
+# CAMERE
 def camera_detail(request, camera_id):
     response = "Questa è la camera %s."
     return HttpResponse(response % camera_id)
+
+
+def camere_list(request):
+    w_camere_list = Camera.objects.order_by("descrizione")[:5]
+    context = {"camere_list": w_camere_list}
+    return render(request, "albdif/camere_list.html", context)
+
+
+# STAGIONI
+def stagione_detail(request, stagione_id):
+    s = get_object_or_404(Stagione, pk=stagione_id)
+    return render(request, "albdif/stagione_detail.html", {"stagione": s})
+
+
+def stagioni_list(request):
+    w_stagioni_list = Stagione.objects.order_by("-data_inizio")[:5]
+    context = {"stagioni_list": w_stagioni_list}
+    return render(request, "albdif/stagioni_list.html", context)
