c2198c07304377e8f09867be51ffe5d0d66e0b6b
fabmeo <164397533+fabmeo@users.noreply.github.com>
2024-12-03 16:21:42 +0100

    WIP form prenotazione, profilo e logout

--- src/siteprj/urls.py
+++ src/siteprj/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = ([
                   path("albdif/", include("albdif.urls")),
                   path('admin/', admin.site.urls),
-              ])
+                  path("accounts/", include("django.contrib.auth.urls")),
+])
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
