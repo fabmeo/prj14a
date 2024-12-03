d4a8015f5bc9acfc0e54d859458eb3ddf6ddb85e
fabmeo <164397533+fabmeo@users.noreply.github.com>
2024-12-03 21:18:25 +0100

    WIP form prenotazione, profilo e logout (#10)

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,22 +1,22 @@
-from django.test import TestCase, Client
+import pytest
 from django.urls import reverse
+from django.test import Client
 
+@pytest.mark.django_db
+class TestMyView:
 
-class MyViewTests(TestCase):
-    # @TODO da rivedere perché non funziona la reverse ...
-    def setUp(self):
+    @pytest.fixture(autouse=True)
+    def setup(self):
         self.client = Client()
 
     def test_home_view(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'albdif/home.html')
-        self.assertContains(response, 'Home Page')
+        response = self.client.get(reverse('albdif:home'))
+        assert response.status_code == 200
+        assert 'albdif/home.html' in [t.name for t in response.templates]
+        assert 'Homepage AD Pegaso' in response.content.decode()
 
-    def test_home_view(self):
-        response = self.client.get(reverse('proprieta_partner'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'albdif/proprieta_list.html')
-        self.assertContains(response, 'Home Page')
-
-
+    def test_proprieta_partner_view(self):
+        response = self.client.get(reverse('albdif:proprieta_partner'))
+        assert response.status_code == 200
+        assert 'albdif/proprieta_list.html' in [t.name for t in response.templates]
+        assert 'Lista dei Partner' in response.content.decode()
