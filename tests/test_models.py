9cf87cd398a2488b3a0aecf15fff02df6d9619e8
fabmeo <164397533+fabmeo@users.noreply.github.com>
2025-01-03 12:23:48 +0100

    aggiunti test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,10 @@
-from albdif.models import Proprieta
+from datetime import date
+from decimal import Decimal
+
+from django_webtest import DjangoTestApp
+
+from albdif.models import Proprieta, Stagione, Camera, PrezzoCamera
+from albdif.utils.fixtures import CameraFactory, StagioneFactory, PrezzoCameraFactory
 
 
 def test_proprieta(user, host):
@@ -16,3 +22,41 @@ def test_proprieta(user, host):
     except Exception as e:
         assert "Esiste già una proprietà principale." in str(e)
 
+
+def test_stagione(app: "DjangoTestApp"):
+    try:
+        StagioneFactory.create(
+            stagione="Bassa",
+            data_inizio=date(2025, 1, 1),
+            data_fine=date(2024, 12, 31)
+        )
+    except Exception as e:
+        assert "La data fine deve essere maggiore della data inizio" in str(e)
+
+    s1 = StagioneFactory.create(
+        stagione="Bassa",
+        data_inizio=date(2025, 1, 1),
+        data_fine=date(2025, 12, 31)
+    )
+    try:
+        s1 = StagioneFactory.create(
+            stagione="Alta",
+            data_inizio=date(2025, 7, 1),
+            data_fine=date(2025, 8, 31),
+            prezzo_default=120
+        )
+    except Exception as e:
+        assert "Le date si sovrappongono ad un'altra stagione" in str(e)
+
+
+def test_camera(app: "DjangoTestApp"):
+    s1 = StagioneFactory.create(
+        stagione="Bassa",
+        data_inizio=date(2025, 1, 1),
+        data_fine=date(2025, 12, 31)
+    )
+    c1 = CameraFactory.create()
+    assert c1.prezzo_bassa_stagione
+    assert c1.prezzo_bassa_stagione > 0
+    PrezzoCameraFactory(camera=c1, stagione=s1, prezzo=45.63)
+    assert c1.prezzo_bassa_stagione == Decimal("45.63")
